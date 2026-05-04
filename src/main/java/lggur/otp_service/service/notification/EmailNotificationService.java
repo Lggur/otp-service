afa993113237f2e705097c32b90f243bcf78763b
Leo Gurzhiev <lggur@mail.ru>
2026-05-04 21:55:32 +0300

    Реализована отправка сообщений по почте

--- src/main/java/lggur/otp_service/service/notification/EmailNotificationService.java
+++ src/main/java/lggur/otp_service/service/notification/EmailNotificationService.java
@@ -1,12 +1,24 @@
 package lggur.otp_service.service.notification;
 
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service("EMAIL")
-    public class EmailNotificationService implements NotificationService {
+public class EmailNotificationService implements NotificationService {
+    private final JavaMailSender mailSender;
 
-        @Override
-        public void send(String destination, String message) {
-            System.out.println("[EMAIL] to=" + destination + " msg=" + message);
-        }
+    public EmailNotificationService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
     }
+
+    @Override
+    public void send(String destination, String message) {
+        SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setTo(destination);
+        mail.setSubject("Your OTP Code");
+        mail.setText(message);
+
+        mailSender.send(mail);
+    }
+}
